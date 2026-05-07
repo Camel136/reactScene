affc591dd3b91d4dfe442904d4b7703e45485dbe
Camel136 <christopherdegasperi@hotmail.com>
2026-05-07 17:56:20 -0300

    ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="app">
-      <header className="section header">Minha UI normal</header>
+      <header className="section header"></header>
 
       <div className="section canvas-container">
         <Canvas shadows>
