11d2ae034cd7da891f8c84b545272eb02fb59064
Camel136 <christopherdegasperi@hotmail.com>
2026-05-06 23:03:20 -0300

    ajuste

--- src/components/house/index.jsx
+++ src/components/house/index.jsx
@@ -5,18 +5,18 @@ import {
   Center,
   TransformControls,
 } from '@react-three/drei';
-import { useMemo } from 'react';
+import { useMemo, useRef } from 'react';
 import * as THREE from 'three';
 // https://gltf.report/
 
 export default function House() {
-  const { nodes } = useGLTF('/gltf/casaRegularJoin.glb');
+  const { nodes } = useGLTF('/gltf/casaRegularJoin1.glb');
   //   console.log('...........', nodes.parede);
   //   atençao ao dar join no blender a ultima geomtria fica como ponto de origem
   const bakedTestureHouse = useTexture('/bake/bakedImage.jpg');
   bakedTestureHouse.flipY = false;
 
-  console.log('...........', nodes);
+  // console.log('...........', nodes);
 
   const glassTableMaterial = useMemo(
     () =>
@@ -34,46 +34,58 @@ export default function House() {
     []
   );
 
-  //   const transformRef = useRef();
-  //   const meshRef = useRef();
+  // const transformRef = useRef();
+  // const meshRef = useRef();
 
-  //   const handleObjectChange = () => {
-  //     const object = transformRef.current?.object;
-  //     console.log('position from control:', object?.position);
-  //   };
+  // const getPosition = () => {
+  //   const object = transformRef.current?.object;
+  //   console.log('position obj', object?.position);
+  // };
+
+  nodes.vidro_mesa.position.x = 0.819228437991848;
+  nodes.vidro_mesa.position.y = -8.817037406785715;
+  nodes.vidro_mesa.position.z = 0.149929950065959;
+
+  nodes.vidro_janela.position.x = 0.819228437991848;
+  nodes.vidro_janela.position.y = -8.817037406785715;
+  nodes.vidro_janela.position.z = 0.149929950065959;
 
   return (
     <>
-      <OrbitControls makeDefault />
-      <group>
-        <color attach="background" args={['#000000']} />
-        <Center>
-          <mesh geometry={nodes.parede.geometry}>
-            <meshBasicMaterial map={bakedTestureHouse} />
-          </mesh>
-        </Center>
-        {/* <PivotControls ref={meshRef} depthTest={false}> */}
-        <TransformControls
-        //   ref={transformRef} onObjectChange={handleObjectChange}
-        >
-          <mesh
-            //   ref={meshRef}
-            geometry={nodes.vidro_mesa.geometry}
-            // material={glassTableMaterial}
-            // position={[
-            //   nodes.vidro_mesa.position.x,
-            //   -nodes.vidro_mesa.position.y,
-            //   nodes.vidro_mesa.position.z,
-            // ]}
-            position={nodes.vidro_mesa.position}
-            rotation={nodes.vidro_mesa.rotation}
-            scale={nodes.vidro_mesa.scale}
-          >
-            <meshBasicMaterial color="red" />
-          </mesh>
-        </TransformControls>
-        {/* </PivotControls> */}
-      </group>
+      <OrbitControls
+        makeDefault
+        minPolarAngle={Math.PI / 4} // limite inferior de rotação vertical (para cima)
+        maxPolarAngle={Math.PI / 2} // limite superior de rotação vertical (para baixo)
+        minAzimuthAngle={-Math.PI / 4} // limite esquerdo de rotação horizontal
+        maxAzimuthAngle={Math.PI / 4} // limite direito de rotação horizontal
+      />
+      <color attach="background" args={['#000000']} />
+      <Center>
+        <mesh geometry={nodes.parede.geometry}>
+          <meshBasicMaterial map={bakedTestureHouse} />
+        </mesh>
+      </Center>
+      {/* <TransformControls
+          ref={transformRef}
+          onObjectChange={getPosition}
+        > */}
+      <mesh
+        // ref={meshRef}
+        geometry={nodes.vidro_mesa.geometry}
+        material={glassTableMaterial}
+        position={nodes.vidro_mesa.position}
+        rotation={nodes.vidro_mesa.rotation}
+        scale={nodes.vidro_mesa.scale}
+      />
+
+      <mesh
+        material={glassTableMaterial}
+        geometry={nodes.vidro_janela.geometry}
+        position={nodes.vidro_janela.position}
+        rotation={nodes.vidro_janela.rotation}
+        scale={nodes.vidro_janela.scale}
+      />
+      {/* </TransformControls> */}
     </>
   );
 }
