3e40673781d40b57d1c118f4f5d24d2466444c7e
Camel136 <christopherdegasperi@hotmail.com>
2026-05-08 10:59:52 -0300

    mobile

--- src/components/controls/index.jsx
+++ src/components/controls/index.jsx
@@ -1,13 +1,34 @@
-import { PointerLockControls } from '@react-three/drei';
+import {
+  PointerLockControls,
+  DeviceOrientationControls,
+} from '@react-three/drei';
+import { useEffect, useState } from 'react';
 
 export default function PointerLockControlsCustom() {
+  const [isMobile, setIsMobile] = useState(false);
+
   const ANGLE = {
-    DEG_30: Math.PI / 6, // 30 g
-    DEG_45: Math.PI / 4, // 45 g
-    DEG_60: Math.PI / 3, // 60 g
-    DEG_90: Math.PI / 2, // 90 g
-    DEG_180: Math.PI, // 180 g
+    DEG_30: Math.PI / 6,
+    DEG_45: Math.PI / 4,
+    DEG_60: Math.PI / 3,
+    DEG_90: Math.PI / 2,
+    DEG_180: Math.PI,
   };
 
-  return <PointerLockControls minPolarAngle={ANGLE.DEG_90} />;
+  useEffect(() => {
+    const mobileRegex =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+    const isMobileDevice = mobileRegex.test(navigator.userAgent);
+
+    const isTouchDevice =
+      'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
+    setIsMobile(isMobileDevice || isTouchDevice);
+  }, []);
+
+  if (!isMobile) {
+    return <PointerLockControls minPolarAngle={ANGLE.DEG_90} />;
+  }
+
+  return <DeviceOrientationControls />;
 }
